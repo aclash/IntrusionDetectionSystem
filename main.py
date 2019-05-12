4ea76c447bfb4921b1c1552b459ca02c8429ee23
aclash <aclash@yeah.net>
2019-05-11 23:23:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from mysql.connector import errorcode
 import smtplib
 import serial
 import time
-#ser = serial.Serial('/dev/ttyUSB0', 9600)
-from_email = 'aclash2009@gmail.com'
-recipients_list = ['aclash@163.com', 'czhang23@horizon.csueastbay.edu']
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+from_email = 'XXX'
+recipients_list = ['XXX', 'XXX']
 cc_list = []
 subject = 'Intrusion Detected!!!'
-message = 'Intrusion Detected! Please click on the link below for more information.'
+message = 'Intrusion Detected! Please click on the link below for more information.\nhttp://74.207.251.237/index.php'
 username = 'XXX'
 password = 'XXX'
 smptserver = 'smtp.gmail.com:587'
@@ -30,7 +30,7 @@ def sendemail(from_email, recipients_list, cc_list, subject, message, username, 
     server.quit()
 
 def takePicture():
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     ret, frame = cap.read()
     timeStamp = time.localtime()
     picName = 'Intrusion' + str(timeStamp) + '.jpg'
@@ -39,7 +39,7 @@ def takePicture():
     cv2.destroyAllWindows()
     timeStamp = time.strftime("%Y-%m-%d %H:%M:%S", timeStamp)
     timeStamp = str(timeStamp)
-    print (timeStamp)
+    print(timeStamp)
     insertBLOB(timeStamp, picName)
     sendemail(from_email, recipients_list, cc_list, subject, message, username, password, smptserver)
 
@@ -48,28 +48,6 @@ def convertToBinaryData(filename):
     with open(filename, 'rb') as file:
         binaryData = file.read()
     return binaryData
-
-def insertFuck(timeStamp, photo):
-    print("Inserting BLOB into intrusionDB table")
-    db = mysql.connector.connect(host = serverIP, database = db_name, user = 'root', password = password)
-    try:
-        cursor = db.cursor(prepared = True)
-        sql_insert_blob_query = """ INSERT INTO `IntrusionData` (`time`, `picture`) VALUES (%s,%s)"""
-        empPicture = convertToBinaryData(photo)
-        # Convert data into tuple format
-        insert_blob_tuple = (timeStamp, empPicture)
-        result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
-        db.commit()
-        print ("Image inserted successfully as a BLOB into IntrusionData table", result)
-    except mysql.connector.Error as error:
-        db.rollback()
-        print("Failed inserting BLOB data into MySQL table {}".format(error))
-    finally:
-        #closing database connection.
-        if(db.is_connected()):
-            cursor.close()
-            db.close()
-            print("MySQL connection is closed")
 
 def insertBLOB(timeStamp, photo):
     print("Inserting BLOB into intrusionDB table")
@@ -82,7 +60,7 @@ def insertBLOB(timeStamp, photo):
         insert_blob_tuple = (timeStamp, empPicture)
         result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
         db.commit()
-        print ("Image inserted successfully as a BLOB into IntrusionData table", result)
+        print("Image inserted successfully as a BLOB into IntrusionData table", result)
     except mysql.connector.Error as error:
         db.rollback()
         print("Failed inserting BLOB data into MySQL table {}".format(error))
@@ -92,6 +70,12 @@ def insertBLOB(timeStamp, photo):
             cursor.close()
             db.close()
             print("MySQL connection is closed")
-
-takePicture()
+            
+#takePicture()
+while (True):
+    line = str(ser.readline())[2:-5]
+    if str(line) == str(1):
+        takePicture()
+    else:
+        print("no motion.")
 
